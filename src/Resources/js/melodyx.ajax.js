dda25a7ef92882f11d4f39f3c05c1537896dbea7
Marek Fojtl <mfojtl98@gmail.com>
2021-12-09 23:37:52 +0100

    Update errors triggered in ajax

--- src/Resources/js/melodyx.ajax.js
+++ src/Resources/js/melodyx.ajax.js
@@ -2,6 +2,23 @@ $.melodyx = {};
 $.melodyx.ajax = function (options, callback) {
     var defaults = {
         success: function (data) {
+            if ('pieces-error' in data) {
+                let show = true;
+                let melodyx = $('#melodyx-ajax-error-wrapper');
+                melodyx.show();
+                melodyx.html(data['pieces-error']);
+                $(document).keyup(function (e) {
+                    if (e.which === 27) {
+                        if (show) {
+                            melodyx.hide();
+                            show = false;
+                        } else {
+                            melodyx.show();
+                            show = true;
+                        }
+                    }
+                })
+            }
             if ('pieces' in data) {
                 let pieces = data.pieces;
                 console.log(pieces);
